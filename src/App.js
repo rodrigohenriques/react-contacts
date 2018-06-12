f7f72c03466edc3a776c11214192731137250d5e
Rodrigo Henriques <rsh.trabalho@gmail.com>
2018-06-11 23:16:10 -0300

    Add props to contact list component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class ContactList extends Component {
+  render() {
+    var people = this.props.contacts
+
+    return <ol>
+      {people.map(person =>
+        <li key={person.name}>
+          {person.name}
+        </li>
+      )}
+    </ol>
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ContactList contacts={[
+          { name: 'Rodrigo' },
+          { name: 'Mozão Linda' }
+        ]} />
+
+        <ContactList contacts={[
+          { name: 'Márcia' },
+          { name: 'Roberto Lemos' }
+        ]} />
       </div>
     );
   }
