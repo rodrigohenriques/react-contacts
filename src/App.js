3cd18dc8c42f0f3b3395445d40821b0795371442
Rodrigo Henriques <rsh.trabalho@gmail.com>
2018-06-12 16:00:35 -0300

    Add styles icons and api file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class ContactList extends Component {
-  render() {
-    var people = this.props.contacts
-
-    return <ol>
-      {people.map(person =>
-        <li key={person.name}>
-          {person.name}
-        </li>
-      )}
-    </ol>
-  }
-}
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Rodrigo' },
-          { name: 'Mozão Linda' }
-        ]} />
-
-        <ContactList contacts={[
-          { name: 'Márcia' },
-          { name: 'Roberto Lemos' }
-        ]} />
-      </div>
+      <div className="App">Hello World!</div>
     );
   }
 }
