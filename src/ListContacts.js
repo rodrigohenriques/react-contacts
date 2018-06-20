53b4a5dcb85fa9640d89ac513c713e02cb17cde0
Rodrigo Henriques <rsh.trabalho@gmail.com>
2018-06-19 23:55:19 -0300

    Convert basic component to stateless functional component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
-class ListContacts extends Component {
-  render() {
-    const { contacts } = this.props
-    return (
-      <ol className='contact-list'>
-        {contacts.map((contact) => (
-          <li key={contact.id} className='contact-list-item'>
-            <div className='contact-avatar' style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }} />
+function ListContacts(props) {
+  return (
+    <ol className='contact-list'>
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className='contact-list-item'>
+          <div className='contact-avatar' style={{
+            backgroundImage: `url(${contact.avatarURL})`
+          }} />
 
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
+          <div className='contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
 
-            <button className='contact-remove'>Remove</button>
-          </li>
-        ))}
-      </ol>
-    )
-  }
+          <button className='contact-remove'>Remove</button>
+        </li>
+      ))}
+    </ol>
+  )
 }
 
 ListContacts.propTypes = {
